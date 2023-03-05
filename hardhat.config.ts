0bd811610045637faad1047bafcf73505cccd97b
wojciech-turek <contact@wojciechturek.com>
2023-03-05 17:32:52 +0100

    Add nahmii network deployment

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -8,13 +8,18 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.17",
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
-  },
+  // etherscan: {
+  //   apiKey: process.env.ETHERSCAN_API_KEY,
+  // },
   networks: {
     "polygon-mainnet": {
       chainId: 137,
       url: "https://rpc-mainnet.maticvigil.com",
+      accounts: [`0x${process.env.PRIVATE_KEY}`],
+    },
+    "nahmii-testnet": {
+      chainId: 4062,
+      url: "https://ngeth.testnet.n3.nahmii.io/",
       accounts: [`0x${process.env.PRIVATE_KEY}`],
     },
   },
